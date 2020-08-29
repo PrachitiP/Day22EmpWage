e21313a00b63622ad85cb6a3199e2e3426baec60
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 17:26:57 +0530

    Loop till Condition

--- Empwage.java
+++ Empwage.java
@@ -1,19 +1,22 @@
-//UseCase 5=>Calculate wage for month
- 
+//UseCase 6=>Loop till condition
+
 class Empwage {
 
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
 	public static final int wagePerHour = 20;
 	public static final int workDayPerMonth = 20;
+	public static final int workHourPerMonth = 100;
 	
 	public static void main(String[] args){
 	
 		int workHour = 0;
 		int monthWage = 0;
+		int workDay = 0;
 
-		for(int i=1;i<=workDayPerMonth;i++){
-		
+		while(workHour < workHourPerMonth && workDay < workDayPerMonth){
+			
+			workDay++;		
 			int check = (int)(Math.random()*3)+1;
 
 			switch(check){
@@ -30,7 +33,7 @@ class Empwage {
 			}
 			int dailyWage = workHour * wagePerHour;
 			monthWage = monthWage + dailyWage;
-			System.out.println("wage of day "+i+" is:"+dailyWage);
+			System.out.println("wage of day "+workDay+" is:"+dailyWage);
 		}
 		System.out.println("Total Month wage is:"+monthWage);
 	}

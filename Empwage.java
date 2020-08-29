acc02bf94bb0405e7e657135ea141159b03f945d
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 17:15:27 +0530

    USing Switch Case

--- Empwage.java
+++ Empwage.java
@@ -1,31 +1,27 @@
-//UseCase 3=>Add Part time and wage
-
-class EmpWageComputation {
+//UseCase 4=>using Swirch case
+ 
+class Empwage {
 
 	public static final int isFullTime = 1;
 	public static final int isPartTime = 2;
 	public static final int wagePerHour = 20;
-	
+
 	public static void main(String[] args){
-	
 		int workHour = 0;
 
 		int check = (int)(Math.random()*3)+1;
 
 		if(check == 1){
-		
+
 			workHour = 8;
 		}
 		else if(check == 2){
-		
 			workHour = 4;
 		}
 		else{
-		
 			workHour = 0;
 		}
 		int dailyWage = workHour * wagePerHour;
 		System.out.println("Daily wage is:"+dailyWage);
 	}
 }
-

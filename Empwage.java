146d30accbd51a9ecf2c46b0699180b9037c8d16
PrachitiP <patilprachiti35@gmail.com>
2020-08-29 16:59:43 +0530

    Check Employee is present or not

--- Empwage.java
+++ Empwage.java
@@ -1,43 +1,17 @@
+//UseCase 1=>check attendance of employee
 class Empwage {
 
-	public static final int isFullTime = 1;
-	public static final int isPartTime = 2;
-	public static final int wagePerHour = 20;
-	
-	public void computeWage(String compName, int workDayPerMonth, int workHourPerMonth){
-	
-		int workHour = 0;
-		int monthWage = 0;
-		int workDay = 0;
+public static void main(String[] args){
 
-		System.out.println("calculating wages for company: "+compName);
-		while(workHour < workHourPerMonth && workDay < workDayPerMonth){
-			
-			workDay++;		
-			int check = (int)(Math.random()*3)+1;
+	int check = (int)(Math.random()*2)+1;
+		if(check == 1){
 
-			switch(check){
-				case isFullTime:
-					workHour = 8;
-					break;
-
-				case isPartTime:
-					workHour = 4;
-					break;
-				default:
-					workHour = 0;
-					break;
-			}
-			int dailyWage = workHour * wagePerHour;
-			monthWage = monthWage + dailyWage;
-			System.out.println("wage of day "+workDay+" is:"+dailyWage);
+			System.out.println("Employee is present");
 		}
-		System.out.println("Total Month wage for company "+compName+" is:"+monthWage);
-	}
-	public static void main(String[] args){
-	
-		EmpWageComputation obj = new EmpWageComputation();
-		obj.computeWage("Reliance",20,100);
-		obj.computeWage("Dmart",25,120);
+		else{
+
+			System.out.println("Employee is absent");
+		}
 	}
 }
+
